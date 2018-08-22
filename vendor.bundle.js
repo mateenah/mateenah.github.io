8af1027c38901ced7fe5761046cd6abc8037588c
mateenah <37463274+mateenah@users.noreply.github.com>
2018-08-22 15:59:08 +0200

    Add files via upload

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -20003,7 +20003,6 @@ var AngularFireDatabaseModule = (function () {
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__public_api__ = __webpack_require__("../../../../angularfire2/database/public_api.js");
 /* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_0__public_api__["a"]; });
-/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_0__public_api__["b"]; });
 
 //# sourceMappingURL=index.js.map
 
@@ -20406,7 +20405,7 @@ function fromRef(ref, event, listenType) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__database__ = __webpack_require__("../../../../angularfire2/database/database.js");
-/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_0__database__["a"]; });
+/* unused harmony namespace reexport */
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__list_changes__ = __webpack_require__("../../../../angularfire2/database/list/changes.js");
 /* unused harmony namespace reexport */
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__list_create_reference__ = __webpack_require__("../../../../angularfire2/database/list/create-reference.js");
@@ -20420,7 +20419,7 @@ function fromRef(ref, event, listenType) {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__observable_fromRef__ = __webpack_require__("../../../../angularfire2/database/observable/fromRef.js");
 /* unused harmony namespace reexport */
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__database_module__ = __webpack_require__("../../../../angularfire2/database/database.module.js");
-/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_7__database_module__["a"]; });
+/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_7__database_module__["a"]; });
 
 
 
